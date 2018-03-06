354f71972476dbe4c6f7d93bd63e1f818b7ede31
fj.piqueras <fj.piqueras@indizen.com>
2018-03-06 12:28:24 +0100

    minor changes

--- src/main/java/com/indizen/fjpiqueras/kafka/util/GeneraEmpleadoRandom.java
+++ src/main/java/com/indizen/fjpiqueras/kafka/util/GeneraEmpleadoRandom.java
@@ -1,6 +1,5 @@
 package com.indizen.fjpiqueras.kafka.util;
 
-import com.indizen.fjpiqueras.kafka.ejercicio3.generator.KafkaRandomGenerator;
 import com.indizen.fjpiqueras.kafka.pojo.Empleado;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -19,13 +18,13 @@ public class GeneraEmpleadoRandom {
 
     public GenericRecord generaEmpleadoRandomAvro() {
 
-        Schema dealMessageSchema = SchemaFactory.getSchemaRegistrySchema("http://127.0.0.1:8081","empleadoSchemaRegistry-value");
-        GenericRecord empleado = new GenericData.Record(dealMessageSchema);
+        Schema empleadoSchema = SchemaFactory.getSchemaRegistrySchema("http://127.0.0.1:8081","empleadoSchemaRegistry-value");
+        GenericRecord empleado = new GenericData.Record(empleadoSchema);
 
         empleado.put("dni", String.valueOf(new Random().nextInt()));
         empleado.put("nombre", getNombreRandom());
         empleado.put("apellidos", getApellidoRandom());
-        empleado.put("email", new Random().nextInt() + "@realmadrid.com");
+        empleado.put("email", new Random().nextInt() + "@indizen.com");
         empleado.put("telefono", new Random().nextInt());
         empleado.put("skills", getSkill(ThreadLocalRandom.current().nextInt(1, 6)));
         empleado.put("estrella", false);
@@ -42,7 +41,7 @@ public class GeneraEmpleadoRandom {
         empleado.setDni(String.valueOf(new Random().nextInt()));
         empleado.setNombre(getNombreRandom());
         empleado.setApellidos(getApellidoRandom());
-        empleado.setEmail(new Random().nextInt() + "@realmadrid.com");
+        empleado.setEmail(new Random().nextInt() + "@indizen.com");
         empleado.setTelefono(new Random().nextInt());
         empleado.setSkills(getSkill(ThreadLocalRandom.current().nextInt(1, 6)));
 
@@ -54,13 +53,13 @@ public class GeneraEmpleadoRandom {
     private String getNombreRandom() {
         List<String> nombres = new ArrayList<>();
 
-        nombres.add("Ronaldo");
-        nombres.add("Karim");
-        nombres.add("Gareth");
-        nombres.add("Mateo");
-        nombres.add("Toni");
-        nombres.add("Keylor");
-        nombres.add("Sergio");
+        nombres.add("García");
+        nombres.add("Pérez");
+        nombres.add("Ruiz");
+        nombres.add("Piqueras");
+        nombres.add("Fernández");
+        nombres.add("Sánchez");
+        nombres.add("González");
 
         return nombres.get(getRandomInt(nombres));
     }
@@ -72,13 +71,13 @@ public class GeneraEmpleadoRandom {
     private String getApellidoRandom() {
         List<String> nombres = new ArrayList<>();
 
-        nombres.add("Nazario");
-        nombres.add("Benzemal");
-        nombres.add("Bale");
-        nombres.add("Kovacic");
-        nombres.add("Kroos");
-        nombres.add("Navas");
-        nombres.add("Ramos");
+        nombres.add("Jose");
+        nombres.add("Javier");
+        nombres.add("Alberto");
+        nombres.add("Aitor");
+        nombres.add("Paco");
+        nombres.add("Dani");
+        nombres.add("Roberto");
 
         return nombres.get(getRandomInt(nombres));
     }
@@ -86,13 +85,13 @@ public class GeneraEmpleadoRandom {
     private List<String> getSkill(int numSkills) {
         List<String> skills = new ArrayList<>();
 
-        skills.add("Goleador");
-        skills.add("Corte");
-        skills.add("Visión de juego");
-        skills.add("Cono");
-        skills.add("Fotogénico");
-        skills.add("Olfato");
-        skills.add("Técnica");
+        skills.add("Front");
+        skills.add("Back");
+        skills.add("Analytics");
+        skills.add("Blockchain");
+        skills.add("Movilidad");
+        skills.add("Big Data");
+        skills.add("PNL");
 
         List<String> skillsJugador = new ArrayList<>();
 
